3c2283ddf35806515ee1258c5d5c6cc514de186e
Sravan T P <sravantp03@gmail.com>
2023-06-02 13:08:54 +0530

    added listEvent function to create new events

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -2,10 +2,58 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenMaster is ERC721 {
+contract TokenMaster is ERC721, Ownable {
+    uint256 private totalEvents;
+
+    struct Event {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    /// Used to keep track of all events
+    mapping(uint256 => Event) public events;
+
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC721(_name, _symbol) {}
+
+    /// Events
+    event CreatedEvent(uint256 indexed _id, string indexed _name);
+
+    function listEvent(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) external {
+        totalEvents++;
+
+        /// Creating Events based on provided data.
+        Event memory newEvent = Event({
+            id: totalEvents,
+            name: _name,
+            cost: _cost,
+            tickets: 0,
+            maxTickets: _maxTickets,
+            date: _date,
+            time: _time,
+            location: _location
+        });
+
+        /// Adding new event to the mapping
+        events[totalEvents] = newEvent;
+
+        emit CreatedEvent(totalEvents, _name);
+    }
 }
--- test/TokenMaster.test.ts
+++ test/TokenMaster.test.ts
@@ -1,22 +1,33 @@
 import { ethers } from "hardhat";
-import { assert, expect } from "chai";
+import { expect } from "chai";
 import { TokenMaster } from "../typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+const TokenName: string = "TokenMaster";
+const TokenSymbol: string = "TM";
 
 describe("Token Master Testing", function () {
     let TokenMaster: TokenMaster;
-    const TokenName: string = "Token Master";
-    const TokenSymbol: string = "TM";
+    let deployer: SignerWithAddress, buyer: SignerWithAddress;
 
     beforeEach(async function () {
-        const TokenMasterFactory = await ethers.getContractFactory("TokenMaster");
+        [deployer, buyer] = await ethers.getSigners();
+        const TokenMasterFactory = await ethers.getContractFactory("TokenMaster", deployer);
         TokenMaster = await TokenMasterFactory.deploy(TokenName, TokenSymbol);
         await TokenMaster.deployed();
     })
 
-    it("should deploy the contract and set name and symbol", async function () {
-        const name: string = await TokenMaster.name();
-        const symbol: string = await TokenMaster.symbol();
-        expect(name).to.be.equal(TokenName);
-        expect(symbol).to.be.equal(TokenSymbol);
+    describe("Deployment", function () {
+        it("Should deploy the contract and set name and symbol", async function () {
+            const name: string = await TokenMaster.name();
+            const symbol: string = await TokenMaster.symbol();
+            expect(name).to.equal(TokenName);
+            expect(symbol).to.equal(TokenSymbol);
+        })
+
+        it ("Should set deployer as the owner of the contract", async function () {
+            const owner = await TokenMaster.owner();
+            expect(owner).to.equal(deployer.address);
+        })
     })
 })
