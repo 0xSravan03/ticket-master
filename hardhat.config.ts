f86c444d64740ad13bba507697e8c4b924d036b4
Sravan T P <sravantp03@gmail.com>
2023-06-03 14:27:16 +0530

    successfully deployed TokenMaster to sepolia network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY as string;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL as string;
+const ETHERSCAN_API = process.env.ETHERSCAN_API as string;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -9,9 +14,12 @@ const config: HardhatUserConfig = {
       url: "",
     },
     sepolia: {
-      url: "",
-      accounts: []
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
   },
   solidity: "0.8.18",
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,14 @@
+// Contract Address : 0x7bD058A0cBBeb65B43487A50cA9E2D09E4BE7b4D
+
 import { ethers } from "hardhat";
 
-async function main() {}
+async function main() {
+  console.log("Deploying TokenMaster contract");
+  const TokenMasterFactory = await ethers.getContractFactory("TokenMaster");
+  const TokenMaster = await TokenMasterFactory.deploy("TokenMaster", "TM");
+  await TokenMaster.deployed();
+  console.log(`TokenMaster contract deployed at ${TokenMaster.address}`);
+}
 
 main().catch((error) => {
   console.error(error);
