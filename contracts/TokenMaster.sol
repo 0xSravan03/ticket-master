9033dafdbd3290cc186e322eb13a70cdef46b170
Sravan T P <sravantp03@gmail.com>
2023-06-03 11:36:43 +0530

    added withdraw function and tests

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenMaster is ERC721, Ownable {
     uint256 public totalEvents;
+    uint256 public totalSupply; /// Keep track of total NFTs.
 
     struct Event {
         uint256 id;
@@ -19,7 +20,10 @@ contract TokenMaster is ERC721, Ownable {
     }
 
     /// Used to keep track of all events
-    mapping(uint256 => Event) public events;
+    mapping(uint256 => Event) private events;
+    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
+    mapping(uint256 => uint256[]) private seatsTaken;
+    mapping(uint256 => mapping(address => bool)) public hasBought;
 
     constructor(
         string memory _name,
@@ -27,8 +31,22 @@ contract TokenMaster is ERC721, Ownable {
     ) ERC721(_name, _symbol) {}
 
     /// Events
-    event CreatedEvent(uint256 indexed _id, string indexed _name);
+    event CreatedEvent(uint256 indexed eventId, string indexed eventName);
+    event PurchaseTicket(
+        uint256 indexed eventId,
+        uint256 indexed seatNumber,
+        address buyer
+    );
 
+    /**
+     * @notice Allow owner of the contract to add new events.
+     * @param _name Name of the Event
+     * @param _cost Cost to buy ticket
+     * @param _maxTickets Maximum allowed tickets to purchase
+     * @param _date Date of the event
+     * @param _time Time of the Event
+     * @param _location Location of the event
+     */
     function listEvent(
         string memory _name,
         uint256 _cost,
@@ -36,7 +54,7 @@ contract TokenMaster is ERC721, Ownable {
         string memory _date,
         string memory _time,
         string memory _location
-    ) external {
+    ) external onlyOwner {
         totalEvents++;
 
         /// Creating Events based on provided data.
@@ -44,7 +62,7 @@ contract TokenMaster is ERC721, Ownable {
             id: totalEvents,
             name: _name,
             cost: _cost,
-            tickets: 0,
+            tickets: _maxTickets,
             maxTickets: _maxTickets,
             date: _date,
             time: _time,
@@ -55,5 +73,61 @@ contract TokenMaster is ERC721, Ownable {
         events[totalEvents] = newEvent;
 
         emit CreatedEvent(totalEvents, _name);
+    }
+
+    /**
+     * @notice This function used to retrieve event details from mapping using id
+     * @param _id  Event id
+     * @return events Event data stored in the events mapping at _id
+     */
+    function getEvent(uint256 _id) public view returns (Event memory) {
+        return events[_id];
+    }
+
+    function mint(uint256 _id, uint256 _seat) public payable {
+        require(_id != 0 && _id <= totalEvents, "Error: Invalid Id");
+        require(
+            _seat > 0 && _seat <= events[_id].maxTickets,
+            "Error: Invalid Seat Number"
+        );
+        require(msg.value >= events[_id].cost, "Error: Insufficient Balance");
+        require(events[_id].tickets > 0, "Error: Tickets Sold Out");
+        require(
+            seatTaken[_id][_seat] == address(0),
+            "Error: Seat has already taken"
+        );
+
+        /// user can buy only one NFT.
+        require(
+            hasBought[_id][msg.sender] == false,
+            "Error: You have already bought"
+        );
+
+        events[_id].tickets--; /// reducing total available ticket count
+
+        hasBought[_id][msg.sender] = true;
+
+        /// Allocating Seat to buyer.
+        seatTaken[_id][_seat] = msg.sender;
+        seatsTaken[_id].push(_seat);
+
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
+
+        emit PurchaseTicket(_id, _seat, msg.sender);
+    }
+
+    function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
+        return seatsTaken[_id];
+    }
+
+    /**
+     * @notice Allow owner of the contract to withdraw eth.
+     */
+    function withdrawFund() external onlyOwner {
+        (bool success, ) = payable(owner()).call{value: address(this).balance}(
+            ""
+        );
+        require(success, "Error: Transaction Failed");
     }
 }
--- test/TokenMaster.test.ts
+++ test/TokenMaster.test.ts
@@ -57,7 +57,7 @@ describe("Token Master Testing", function () {
             const afterEventCount = await TokenMaster.totalEvents();
             expect(afterEventCount).to.equal(1);
 
-            const eventDetails = await TokenMaster.events(1);
+            const eventDetails = await TokenMaster.getEvent(1);
 
             expect(eventDetails.id).to.equal(1);
             expect(eventDetails.name).to.equal(Event_Name);
@@ -66,6 +66,168 @@ describe("Token Master Testing", function () {
             expect(eventDetails.date).to.equal(Event_Date);
             expect(eventDetails.time).to.equal(Event_Time);
             expect(eventDetails.location).to.equal(Event_Location);
+        })
+
+        it("Only owner should be able to create a new event", async function () {
+            const [, , UnknownUser] = await ethers.getSigners();
+
+            const Event_Name: string = "XYZ Music Concert";
+            const Event_Cost: BigNumber = ethers.utils.parseEther("0.1");
+            const Event_MaxTickets: number = 10000;
+            const Event_Date: string = "10-06-2023";
+            const Event_Time: string = "10:30 PM, IST";
+            const Event_Location: string = "MUMBAI, INDIA";
+
+            await expect(TokenMaster.connect(UnknownUser).listEvent(
+                Event_Name,
+                Event_Cost,
+                Event_MaxTickets,
+                Event_Date,
+                Event_Time,
+                Event_Location
+            )).revertedWith("Ownable: caller is not the owner");
+        })
+    })
+
+    describe("Mint", function () {
+        beforeEach(async function () {
+            const Event_Name: string = "XYZ Music Concert";
+            const Event_Cost: BigNumber = ethers.utils.parseEther("0.1");
+            const Event_MaxTickets: number = 10000;
+            const Event_Date: string = "10-06-2023";
+            const Event_Time: string = "10:30 PM, IST";
+            const Event_Location: string = "MUMBAI, INDIA";
+
+            const tx = await TokenMaster.listEvent(
+                Event_Name,
+                Event_Cost,
+                Event_MaxTickets,
+                Event_Date,
+                Event_Time,
+                Event_Location
+            )
+            await tx.wait();
+        })
+
+        it("User should able to purchase tickets", async function () {
+            const Event_Id: number = 1;
+            const Seat_Number: number = 10;
+            const Price: BigNumber = ethers.utils.parseEther("0.1");
+
+            const tx = await TokenMaster.connect(buyer).mint(Event_Id, Seat_Number, 
+                { value: Price }
+            );
+            await tx.wait();
+
+            const eventDetails = await TokenMaster.getEvent(1);
+            expect(eventDetails.tickets).to.equal(eventDetails.maxTickets.sub(1));
+            expect(await TokenMaster.seatTaken(Event_Id, Seat_Number)).to.equal(buyer.address);
+            expect(await TokenMaster.totalSupply()).to.equal(1);
+        })
+
+        it("Should revert if not enough eth sent", async function () {
+            await expect(TokenMaster.connect(buyer).mint(1, 5)).to.revertedWith("Error: Insufficient Balance");
+        })
+
+        it("Should set buying status to true after purchase", async function () {
+            const Event_Id: number = 1;
+            const Seat_Number: number = 10;
+            const Price: BigNumber = ethers.utils.parseEther("0.1");
+
+            const tx = await TokenMaster.connect(buyer).mint(Event_Id, Seat_Number, 
+                { value: Price }
+            );
+            await tx.wait();
+
+            expect(await TokenMaster.connect(buyer).hasBought(1, buyer.address)).to.equal(true);
+        })
+
+        it("Should update seatsTaken array", async function () {
+            const Event_Id: number = 1;
+            const Seat_Number: number = 10;
+            const Price: BigNumber = ethers.utils.parseEther("0.1");
+
+            const tx = await TokenMaster.connect(buyer).mint(Event_Id, Seat_Number, 
+                { value: Price }
+            );
+            await tx.wait();
+
+            const seats = await TokenMaster.connect(buyer).getSeatsTaken(Event_Id);
+            expect(seats.length).to.equal(1);
+            expect(seats[0]).to.equal(Seat_Number);
+        })
+
+        it("Should update the contract balance", async function () {
+            const Event_Id: number = 1;
+            const Seat_Number: number = 10;
+            const Price: BigNumber = ethers.utils.parseEther("0.1");
+
+            const tx = await TokenMaster.connect(buyer).mint(Event_Id, Seat_Number, 
+                { value: Price }
+            );
+            await tx.wait();
+
+            const contractBalance = await ethers.provider.getBalance(TokenMaster.address);
+            expect(contractBalance).to.equal(Price);
+        })
+
+        it("Should not able to purchase ticket for invalid event id", async function () {
+            await expect(TokenMaster.connect(buyer)
+            .mint(2, 8, {value: ethers.utils.parseEther("0.1")}))
+            .to.revertedWith("Error: Invalid Id");
+        })
+
+        it("Should not able to purchase invalid seat", async function () {
+            await expect(TokenMaster.connect(buyer).
+            mint(1, 10001, {value: ethers.utils.parseEther("0.1")}))
+            .to.revertedWith("Error: Invalid Seat Number");
+        })
+    })
+
+    describe("Withdraw", function () {
+        beforeEach(async function () {
+            const Event_Name: string = "XYZ Music Concert";
+            const Event_Cost: BigNumber = ethers.utils.parseEther("1");
+            const Event_MaxTickets: number = 10000;
+            const Event_Date: string = "10-06-2023";
+            const Event_Time: string = "10:30 PM, IST";
+            const Event_Location: string = "MUMBAI, INDIA";
+
+            const tx = await TokenMaster.listEvent(
+                Event_Name,
+                Event_Cost,
+                Event_MaxTickets,
+                Event_Date,
+                Event_Time,
+                Event_Location
+            )
+            await tx.wait();
+        })
+
+        it("Owner should able to withdraw the eth from contract", async function () {
+            const ownerBalance1 = await deployer.getBalance();
+            let contractBalance = await ethers.provider.getBalance(TokenMaster.address);
+            expect(contractBalance).to.equal(0);
+
+            const tx = await TokenMaster.connect(buyer).mint(1, 10, 
+                { value: ethers.utils.parseEther("1") }
+            );
+            await tx.wait();
+            contractBalance = await ethers.provider.getBalance(TokenMaster.address);
+
+            expect(contractBalance).to.equal(ethers.utils.parseEther("1"))
+
+            const tx1 = await TokenMaster.connect(deployer).withdrawFund();
+            const tx1Receipt = await tx1.wait();
+
+            const gasUsed = tx1Receipt.gasUsed;
+            const effectiveGasPrice = tx1Receipt.effectiveGasPrice;
+            const gasFee = gasUsed.mul(effectiveGasPrice);
+
+            const newOwnerBalance = await deployer.getBalance();
+
+            expect(await ethers.provider.getBalance(TokenMaster.address)).to.equal(0);
+            expect(newOwnerBalance).to.equal(ownerBalance1.add(ethers.utils.parseEther("1")).sub(gasFee));
         })
     })
 })
